7d68c3ae544fa02a039d055120c1e05bbd423b90
Bence Vajda <bence.vajda@me.com>
2013-04-27 17:14:08 +0200

    moves the #config tests to own file: the line_up specs don't need to run in a specific order, and the spec directory structure reflects the structure of lib

---
+++ spec/lib/line_up/configuration_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe LineUp do
+
+  let(:logger)      { mock(:logger) }
+
+  let(:lineup) { LineUp }
+
+  describe '.config' do
+    before do
+      LineUp.reset!
+    end
+
+    it 'is an STDOUT logger' do
+      Logger.should_receive(:new).with(STDOUT).and_return logger
+      lineup.config.logger.should be logger
+    end
+
+    context 'with Rails' do
+      before do
+        ensure_module :Rails
+        Rails.stub!(:logger).and_return(logger)
+      end
+
+      it 'is the Rails logger' do
+        lineup.config.logger.should be Rails.logger
+      end
+    end
+  end
+end
