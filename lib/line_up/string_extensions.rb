c99df0b8eeb7601c69000263fb4a1635beafb930
Bence Vajda <vajda@it.auktionskompaniet.com>
2013-05-07 13:30:59 +0200

    don't convert nil to string

--- lib/line_up/string_extensions.rb
+++ lib/line_up/string_extensions.rb
@@ -3,12 +3,14 @@ module LineUp
 
     # See https://github.com/rails/rails/blob/master/activesupport/lib/active_support/inflector/methods.rb#L90
     def self.underscore(word)
-      word = word.to_s
-      word.gsub!(/::/, '/')
-      word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-      word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
-      word.tr!("-", "_")
-      word.downcase!
+      unless word.nil?
+        word = word.to_s
+        word.gsub!(/::/, '/')
+        word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+        word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+        word.tr!("-", "_")
+        word.downcase!
+      end
       word
     end
 
